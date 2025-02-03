a0a981e6cf40856d1ca95bcfabe2ef09ffaaea3e
Juan Pablo Muñoz <juan.munoz@reorg.com>
2025-02-03 10:15:04 -0500

    unfollow functionalities

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,17 +1,47 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    IsFollowing: true
+  },
+  {
+    userName: 'juanmunoz',
+    name: 'Juan Muñoz',
+    IsFollowing: true
+  },
+  {
+    userName: 'davidbonilla',
+    name: 'David Bonilla'
+  },
+  {
+    userName: 'josemadan',
+    name: 'Jose Madan'
+  },
+  {
+    userName: 'pabloalbaladejo',
+    name: 'Pablo Albaladejo'
+  }
+]
+
 export function App() {
 
   return (
     <section className='App'>
-      <TwitterFollowCard userName="midudev">
-        Miguel Ángel Durán
-      </TwitterFollowCard>
-
-      <TwitterFollowCard initialIsFollowing userName="juanmunoz">
-        Juan Muñoz
-      </TwitterFollowCard>
+      {
+        users.map(({ userName, name, IsFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={IsFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        )
+        )
+      }
     </section>
   )
 }
